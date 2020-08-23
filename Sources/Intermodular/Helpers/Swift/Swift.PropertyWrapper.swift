ac13aff32f2e7cd12d08cc3e71ee18228ebb7310
Vatsal Manot <vatsal.manot@yahoo.com>
2020-08-23 19:40:57 +0530

    Update package

--- Sources/Intermodular/Helpers/Swift/Swift.PropertyWrapper.swift
+++ Sources/Intermodular/Helpers/Swift/Swift.PropertyWrapper.swift
@@ -4,8 +4,19 @@
 
 import Swift
 
+/// A type-erased shadow protocol for `PropertyWrapper`.
+public protocol _opaque_PropertyWrapper {
+    var _opaque_wrappedValue: Any { get }
+}
+
+extension _opaque_PropertyWrapper where Self: PropertyWrapper {
+    public var _opaque_wrappedValue: Any {
+        wrappedValue
+    }
+}
+
 /// A protocol formalizing a `@propertyWrapper`.
-public protocol PropertyWrapper {
+public protocol PropertyWrapper: _opaque_PropertyWrapper {
     associatedtype WrappedValue
     
     var wrappedValue: WrappedValue { get }
