9f686ef305b6c83179ace64eaa26a1b3877a640c
Vatsal Manot <vatsal.manot@yahoo.com>
2020-09-07 02:20:44 +0530

    Update package

--- Sources/Intermodular/Extensions/Swift/String+Substring.swift
+++ Sources/Intermodular/Extensions/Swift/String+Substring.swift
@@ -8,22 +8,22 @@ extension String {
     public func contains(substring: Substring) -> Bool {
         return substring.parent == self && bounds.contains(substring.bounds)
     }
-
+    
     public func contains(substring: Substring?) -> Bool {
         return substring.map({ contains(substring: $0) }) ?? false
     }
-
+    
     public subscript(firstSubstring substring: String) -> Substring? {
         return range(of: substring).map({ self[$0] })
     }
-
+    
     public func substrings(separatedBy separator: Character) -> [Substring] {
         return split(whereSeparator: { $0 == separator })
     }
     
     public func lines() -> [Substring] {
         TODO.whole(.addressEdgeCase, .optimize, note: "Address \"\r\\n\" because Microsoft fucking Windows")
-
+        
         return substrings(separatedBy: Character.newLine)
     }
     
@@ -40,8 +40,15 @@ extension String {
     }
     
     public func dropPrefixIfPresent<String: StringProtocol>(_ prefix: String) -> Substring {
-        return hasPrefix(prefix) ? dropFirst(prefix.count) : .init(self)
+        hasPrefix(prefix) ? dropFirst(prefix.count) : .init(self)
     }
+    
+    #if swift(>=5.3)
+    @_disfavoredOverload
+    public func dropPrefixIfPresent<String: StringProtocol>(_ prefix: String) -> Self {
+        Self(dropPrefixIfPresent(prefix) as Substring)
+    }
+    #endif
     
     public func dropLast() -> Substring {
         return dropLast(1)
@@ -50,6 +57,13 @@ extension String {
     public func dropSuffixIfPresent<String: StringProtocol>(_ suffix: String) -> Substring {
         return hasSuffix(suffix) ? dropLast(suffix.count) : .init(self)
     }
+    
+    #if swift(>=5.3)
+    @_disfavoredOverload
+    public func dropSuffixIfPresent<String: StringProtocol>(_ prefix: String) -> Self {
+        Self(dropSuffixIfPresent(prefix) as Substring)
+    }
+    #endif
 }
 
 extension Substring {
