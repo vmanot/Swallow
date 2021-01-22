1c23f4758bce91abc030516e99ffcc08a210f653
Vatsal Manot <vatsal.manot@yahoo.com>
2021-01-22 14:50:56 +0530

    Update package

--- Sources/Intermodular/Extensions/Swift/KeyedDecodingContainerProtocol++.swift
+++ Sources/Intermodular/Extensions/Swift/KeyedDecodingContainerProtocol++.swift
@@ -6,20 +6,26 @@ import Swift
 
 extension KeyedDecodingContainerProtocol {
     public func decode(opaque type: Decodable.Type, forKey key: Key) throws -> Decodable {
-        return try type.decode(from: self, forKey: key)
+        try type.decode(from: self, forKey: key)
     }
-
+    
     public func decodeIfPresent(opaque type: Decodable.Type, forKey key: Key) throws -> Decodable? {
-        return try type.decodeIfPresent(from: self, forKey: key)
+        try type.decodeIfPresent(from: self, forKey: key)
     }
 }
 
 extension KeyedDecodingContainerProtocol {
     public func decode<T: Decodable>(forKey key: Key) throws -> T {
-        return try decode(T.self, forKey: key)
+        try decode(T.self, forKey: key)
     }
     
     public func decodeIfPresent<T: Decodable & OptionalProtocol>(forKey key: Key) throws -> T where T.Wrapped: Decodable {
-        return .init(try decodeIfPresent(T.Wrapped.self, forKey: key))
+        .init(try decodeIfPresent(T.Wrapped.self, forKey: key))
+    }
+}
+
+extension KeyedDecodingContainerProtocol {
+    public func decoder(forKey key: Key) throws -> Decoder {
+        try decode(DecoderUnwrapper.self, forKey: key).value
     }
 }
--- Sources/Intermodular/Extensions/Swift/KeyedEncodingContainerProtocol++.swift
+++ Sources/Intermodular/Extensions/Swift/KeyedEncodingContainerProtocol++.swift
@@ -8,12 +8,18 @@ extension KeyedEncodingContainerProtocol {
     public mutating func encode(opaque value: Encodable, forKey key: Key) throws {
         try value.encode(to: &self, forKey: key)
     }
-
+    
     public mutating func encodeIfPresent(opaque value: Encodable?, forKey key: Key) throws {
         guard let value = value else {
             return
         }
-
+        
         try encode(opaque: value, forKey: key)
+    }
+}
+
+extension KeyedEncodingContainerProtocol {
+    public mutating func encode(using encodeImpl: @escaping (Encoder) throws -> (), forKey key: Key) throws {
+        try encode(EncodableImpl(encodeImpl), forKey: key)
     }
 }
