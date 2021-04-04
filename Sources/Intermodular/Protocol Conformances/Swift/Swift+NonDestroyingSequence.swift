92989072b109db087a8b046189a146bb84e83d14
Vatsal Manot <vatsal.manot@yahoo.com>
2021-04-05 01:15:00 +0530

    Update package

--- Sources/Intermodular/Protocol Conformances/Swift/Swift+NonDestroyingSequence.swift
+++ Sources/Intermodular/Protocol Conformances/Swift/Swift+NonDestroyingSequence.swift
@@ -5,49 +5,85 @@
 import Swift
 
 extension Array: NonDestroyingMutableRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension ContiguousArray: NonDestroyingMutableRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension CollectionOfOne: NonDestroyingMutableRandomAccessCollection {
-
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension Dictionary: NonDestroyingCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension Set: NonDestroyingSequence {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension String: NonDestroyingBidirectionalCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension String.UnicodeScalarView: NonDestroyingBidirectionalCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension Substring: NonDestroyingBidirectionalCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension UnsafeBufferPointer: NonDestroyingRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension UnsafeMutableBufferPointer: NonDestroyingMutableRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension UnsafeRawBufferPointer: NonDestroyingRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
 
 extension UnsafeMutableRawBufferPointer: NonDestroyingMutableRandomAccessCollection {
-    
+    @inlinable
+    public var nonDestructiveCount: Int {
+        count
+    }
 }
