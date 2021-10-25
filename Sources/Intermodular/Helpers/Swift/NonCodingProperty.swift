91cc73966c2d944853fb3d748ab0630cefc4a80e
Vatsal Manot <vatsal.manot@yahoo.com>
2021-10-25 22:58:38 +0530

    Update package

--- Sources/Intermodular/Helpers/Swift/NonCodingProperty.swift
+++ Sources/Intermodular/Helpers/Swift/NonCodingProperty.swift
@@ -6,7 +6,7 @@ import Swift
 
 /// A property that does not encode/decode itself.
 @propertyWrapper
-public struct TransientProperty<Value: Initiable>: PropertyWrapper {
+public struct NonCodingProperty<Value: ExpressibleByNilLiteral>: PropertyWrapper {
     public var wrappedValue: Value
     
     public init(wrappedValue: Value) {
@@ -16,15 +16,15 @@ public struct TransientProperty<Value: Initiable>: PropertyWrapper {
 
 // MARK: - Conformances -
 
-extension TransientProperty: Equatable where Value: Equatable {
+extension NonCodingProperty: Equatable where Value: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.wrappedValue == rhs.wrappedValue
     }
 }
 
-extension TransientProperty: Codable {
+extension NonCodingProperty: Codable {
     public init(from decoder: Decoder) throws {
-        self.init(wrappedValue: .init())
+        self.init(wrappedValue: .init(nilLiteral: ()))
     }
     
     public func encode(to encoder: Encoder) throws {
@@ -32,7 +32,7 @@ extension TransientProperty: Codable {
     }
 }
 
-extension TransientProperty: Hashable where Value: Hashable {
+extension NonCodingProperty: Hashable where Value: Hashable {
     public func hash(into hasher: inout Hasher) {
         wrappedValue.hash(into: &hasher)
     }
