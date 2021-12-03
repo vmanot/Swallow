888471b251f5e846692c6998aab128183a3255bd
Vatsal Manot <vatsal.manot@yahoo.com>
2021-12-03 22:50:08 +0530

    Update package

--- Sources/Intermodular/Helpers/_opaque_Swift/_opaque_Hashable.swift
+++ Sources/Intermodular/Helpers/_opaque_Swift/_opaque_Hashable.swift
@@ -19,3 +19,20 @@ extension _opaque_Hashable where Self: Hashable {
         return .init(self)
     }
 }
+
+// MARK: - Auxiliary Implementation -
+
+@_disfavoredOverload
+public func == (lhs: AnyHashable, rhs: _opaque_Hashable) -> Bool {
+    lhs == rhs.toAnyHashable()
+}
+
+@_disfavoredOverload
+public func == <T: Hashable>(lhs: _opaque_Hashable, rhs: T) -> Bool {
+    lhs.toAnyHashable() == AnyHashable(rhs)
+}
+
+@_disfavoredOverload
+public func == <T: _opaque_Hashable, U: Hashable>(lhs: T, rhs: U) -> Bool {
+    lhs.toAnyHashable() == AnyHashable(rhs)
+}
