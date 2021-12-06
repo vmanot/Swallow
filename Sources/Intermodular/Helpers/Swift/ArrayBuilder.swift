4047d57859e4e8953f2df40b1d88dffe171130ec
Vatsal Manot <vatsal.manot@yahoo.com>
2021-12-06 15:34:48 +0530

    Update package

--- Sources/Intermodular/Helpers/Swift/ArrayBuilder.swift
+++ Sources/Intermodular/Helpers/Swift/ArrayBuilder.swift
@@ -4,40 +4,25 @@
 
 import Swift
 
-#if swift(<5.4)
-@_functionBuilder
-open class ArrayBuilder<Element> {
-    @inlinable
-    public static func buildBlock() -> [Element] {
-        return []
-    }
-    
-    @inlinable
-    public static func buildBlock(_ element: Element) -> [Element] {
-        return [element]
-    }
-    
-    @inlinable
-    public static func buildBlock(_ elements: Element...) -> [Element] {
-        return elements
-    }
-}
-#else
 @resultBuilder
 open class ArrayBuilder<Element> {
     @inlinable
     public static func buildBlock() -> [Element] {
-        return []
+        []
     }
-    
+
     @inlinable
     public static func buildBlock(_ element: Element) -> [Element] {
-        return [element]
+        [element]
     }
-    
+
     @inlinable
     public static func buildBlock(_ elements: Element...) -> [Element] {
-        return elements
+        elements
+    }
+
+    @inlinable
+    public static func buildBlock(_ elements: [Element]) -> [Element] {
+        elements
     }
 }
-#endif
