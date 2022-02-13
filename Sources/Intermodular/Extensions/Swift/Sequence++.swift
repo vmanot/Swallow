f719bedc7b9dd33d17aa8bb334b52f54315ba78a
Vatsal Manot <vatsal.manot@yahoo.com>
2022-02-14 02:48:07 +0530

    Update package

--- Sources/Intermodular/Extensions/Swift/Sequence++.swift
+++ Sources/Intermodular/Extensions/Swift/Sequence++.swift
@@ -75,6 +75,109 @@ extension Sequence {
     }
 }
 
+// MARK: - concurrentForEach -
+
+extension Sequence where Element: Sendable {
+    public func concurrentForEach(
+        _ operation: @escaping @Sendable (Element) async -> Void
+    ) async {
+        await withTaskGroup(of: Void.self) { group in
+            for element in self {
+                group.addTask {
+                    await operation(element)
+                }
+            }
+
+            await group.waitForAll()
+        }
+    }
+
+    public func concurrentForEach(
+        _ operation: @escaping @Sendable (Element) async throws -> Void
+    ) async throws {
+        try await withThrowingTaskGroup(of: Void.self) { group in
+            for element in self {
+                group.addTask {
+                    try await operation(element)
+                }
+            }
+
+            try await group.waitForAll()
+        }
+    }
+}
+
+// MARK: concurrentMap
+
+extension Sequence where Element: Sendable {
+    /// Returns an array containing the results of mapping the given async closure over
+    /// the sequence’s elements.
+    ///
+    /// The closure calls are made serially. The next call is only made once the previous call
+    /// has finished. Returns once the closure has run on all the elements of the Sequence
+    /// or when the closure throws an error.
+    /// - Parameter transform: An async  mapping closure. transform accepts an
+    ///     element of this sequence as its parameter and returns a transformed value of
+    ///     the same or of a different type.
+    /// - Returns: An array containing the transformed elements of this sequence.
+    public func asyncMap<T: Sendable>(_ transform: @Sendable @escaping (Element) async throws -> T) async rethrows -> [T] {
+        let initialCapacity = underestimatedCount
+        var result = ContiguousArray<T>()
+        result.reserveCapacity(initialCapacity)
+
+        for element in self {
+            try await result.append(transform(element))
+        }
+        return Array(result)
+    }
+
+    /// Returns an array containing the results of mapping the given async closure over
+    /// the sequence’s elements.
+    ///
+    /// This differs from `asyncMap` in that it uses a `TaskGroup` to run the transform
+    /// closure for all the elements of the Sequence. This allows all the transform closures
+    /// to run concurrently instead of serially. Returns only when the closure has been run
+    /// on all the elements of the Sequence.
+    /// - Parameters:
+    ///   - priority: Task priority for tasks in TaskGroup
+    ///   - transform: An async mapping closure. transform accepts an
+    ///     element of this sequence as its parameter and returns a transformed value of
+    ///     the same or of a different type.
+    /// - Returns: An array containing the transformed elements of this sequence.
+    public func concurrentMap<T: Sendable>(
+        priority: TaskPriority? = nil,
+        _ transform: @Sendable @escaping (Element) async throws -> T
+    ) async rethrows -> [T] {
+        try await withThrowingTaskGroup(of: (Int, T).self) { group in
+            enumerated().forEach { element in
+                group.addTask(priority: priority) {
+                    let result = try await transform(element.1)
+
+                    return (element.0, result)
+                }
+            }
+
+            let initialCapacity = underestimatedCount
+
+            var result = ContiguousArray<(Int, T)>()
+
+            result.reserveCapacity(initialCapacity)
+
+            for _ in 0..<initialCapacity {
+                try await result.append(group.next()!)
+            }
+
+            while let element = try await group.next() {
+                result.append(element)
+            }
+
+            try await group.waitForAll()
+
+            return result.sorted(by: { $0.0 < $1.0 }).map({ $0.1 })
+        }
+    }
+}
+
 // MARK: duplicate
 
 extension Sequence {
