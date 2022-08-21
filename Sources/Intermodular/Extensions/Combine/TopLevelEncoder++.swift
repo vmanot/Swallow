cce1dcb927c5874d97d86f07c99400847d1c03d0
Vatsal Manot <vatsal.manot@yahoo.com>
2022-08-22 04:11:22 +0530

    Update package

---
+++ Sources/Intermodular/Extensions/Combine/TopLevelEncoder++.swift
@@ -0,0 +1,22 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Combine
+import Swift
+
+extension TopLevelEncoder {
+    /// Encodes an instance of the indicated type, if it is encodable.
+    @_disfavoredOverload
+    public func encode<T>(_ value: T) throws -> Output {
+        try cast(value, to: Encodable.self).encode(using: self)
+    }
+}
+
+// MARK: - Auxiliary Implementation -
+
+extension Encodable {
+    fileprivate func encode<Encoder: TopLevelEncoder>(using encoder: Encoder) throws -> Encoder.Output {
+        try encoder.encode(self)
+    }
+}
