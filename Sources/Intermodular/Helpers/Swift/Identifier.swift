d1512db9bf8ba7fd20c9926314b465e12cf2374e
Vatsal Manot <vatsal.manot@yahoo.com>
2022-10-14 15:53:38 +0530

    Remove unused code

--- Sources/Intermodular/Helpers/Swift/Identifier.swift
+++ Sources/Intermodular/Helpers/Swift/Identifier.swift
@@ -25,40 +25,6 @@ extension AnyHashable: Identifier {
     
 }
 
-public struct AnyIdentifier: Identifier {
-    public typealias Value = _opaque_Identifier
-    
-    public let value: Value
-    
-    public init(_ value: Value) {
-        self.value = value
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(opaque: value)
-    }
-    
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.value._opaque_Equatable_isEqual(to: rhs.value) ?? false
-    }
-}
-
-public struct AnyStringIdentifier: Codable, Identifier {
-    public let value: String
-    
-    public init(_ value: String) {
-        self.value = value
-    }
-    
-    public init(from decoder: Decoder) throws {
-        self.init(try .init(from: decoder))
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        try value.encode(to: encoder)
-    }
-}
-
 extension ObjectIdentifier: Identifier {
     
 }
