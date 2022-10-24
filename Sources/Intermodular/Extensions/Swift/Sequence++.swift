b9188c9592118e5f7c6e04d823a253f6bc9f20b8
Vatsal Manot <vatsal.manot@yahoo.com>
2022-10-24 14:41:43 +0530

    Add Sequence.splitBefore

--- Sources/Intermodular/Extensions/Swift/Sequence++.swift
+++ Sources/Intermodular/Extensions/Swift/Sequence++.swift
@@ -559,3 +559,29 @@ extension Sequence where Element: Hashable {
         Array(self.distinct() as AnySequence)
     }
 }
+
+// MARK: - splitBefore -
+
+extension Sequence {
+    public func splitBefore(
+        separator isSeparator: (Iterator.Element) throws -> Bool
+    ) rethrows -> [AnySequence<Iterator.Element>] {
+        var result: [AnySequence<Iterator.Element>] = []
+        var subSequence: [Iterator.Element] = []
+        
+        var iterator = self.makeIterator()
+        while let element = iterator.next() {
+            if try isSeparator(element) {
+                if !subSequence.isEmpty {
+                    result.append(AnySequence(subSequence))
+                }
+                subSequence = [element]
+            }
+            else {
+                subSequence.append(element)
+            }
+        }
+        result.append(AnySequence(subSequence))
+        return result
+    }
+}
