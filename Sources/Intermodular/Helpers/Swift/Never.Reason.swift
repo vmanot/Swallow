fa6ce73ec9bf32f37151fd91b47c179433a2b965
Vatsal Manot <vatsal.manot@yahoo.com>
2023-04-24 13:44:20 +0530

    Update package

--- Sources/Intermodular/Helpers/Swift/Never.Reason.swift
+++ Sources/Intermodular/Helpers/Swift/Never.Reason.swift
@@ -8,18 +8,72 @@ extension Never {
     /// A reason for why something returns `Never`.
     ///
     /// This type is a work-in-progress. Do not use this type directly in your code.
-    public enum Reason: Error {
-        case abstract
-        case functionFailure
-        case illegal
-        case impossible
-        case irrational
-        case osUnsupported
-        case unavailable
-        case unimplemented
-        case unsupported
+    public struct Reason: Hashable, Error {
+        public enum _Base: String, Hashable {
+            case abstract
+            case illegal
+            case impossible
+            case irrational
+            case osUnsupported
+            case unavailable
+            case unimplemented
+            case unsupported
+        }
+        
+        public let _base: _Base
+        
+        @_transparent
+        public init(_base base: _Base) {
+            self._base = base
+            
+            runtimeIssue("This code path should never be invoked (reason: \(base.rawValue)).")
+        }
+    }
+}
+
+extension Never.Reason {
+    @_transparent
+    public static var abstract: Self {
+        .init(_base: .abstract)
     }
     
+    @_transparent
+    public static var illegal: Self {
+        .init(_base: .illegal)
+    }
+    
+    @_transparent
+    public static var impossible: Self {
+        .init(_base: .impossible)
+    }
+    
+    @_transparent
+    public static var irrational: Self {
+        .init(_base: .irrational)
+    }
+    
+    @_transparent
+    public static var osUnsupported: Self {
+        .init(_base: .osUnsupported)
+    }
+    
+    @_transparent
+    public static var unavailable: Self {
+        .init(_base: .unavailable)
+    }
+    
+    @_transparent
+    public static var unimplemented: Self {
+        .init(_base: .unimplemented)
+    }
+    
+    @_transparent
+    public static var unsupported: Self {
+        .init(_base: .unsupported)
+    }
+}
+
+extension Never {
     public static func materialize() -> Never {
         fatalError()
     }
@@ -37,8 +91,6 @@ extension Never {
         switch reason {
             case .abstract:
                 fatalError("abstract method called", file: file, line: line)
-            case .functionFailure:
-                fatalError(String(describing: function) + " failure", file: file, line: line)
             case .illegal:
                 fatalError("illegal", file: file, line: line)
             case .impossible:
@@ -53,6 +105,8 @@ extension Never {
                 fatalError("\(function) unimplemented", file: file, line: line)
             case .unsupported:
                 fatalError("\(function) unsupported", file: file, line: line)
+            default:
+                fatalError()
         }
     }
     
---
+++ Sources/Intramodular/Logging/XcodeRuntimeIssueLogger.swift
@@ -0,0 +1,157 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Foundation
+import os.log
+#if DEBUG
+import _SwiftOSOverlayShims
+#endif
+
+public struct XcodeRuntimeIssueLogger {
+    /// Returns the shared default runtime issue logger with a generic category.
+    public static let `default` = Self(category: "Runtime issues")
+    
+    private static let commonSubsystem = "com.apple.runtime-issues"
+    
+    @usableFromInline
+    let log: OSLog
+    @usableFromInline
+    let callsiteCache = CallsiteCache()
+        
+    /// Initializes a custom runtime issue logger with a custom category.
+    public init(category: StaticString) {
+        self.log = OSLog(subsystem: Self.commonSubsystem, category: String(_staticString: category))
+    }
+    
+    /// Log a runtime issue to the console.
+    ///
+    /// When executed while attached to Xcode's debugger, this will have the additional effect
+    /// of highlighting the issue and providing heads-up information regarding the issue.
+    @_transparent
+    public func raise(
+        _ warningFormat: StaticString,
+        file: StaticString = #file,
+        line: UInt = #line,
+        vaList: CVarArg...
+    ) {
+        guard _isDebugAssertConfiguration else {
+            return
+        }
+        
+        guard log.isEnabled(type: .fault), callsiteCache.shouldRaiseIssue(in: file, on: line) else {
+            return
+        }
+        
+        guard let handle = XcodeRuntimeIssueLogger.systemFrameworkHandle else {
+            return RTI_RUNTIME_ISSUES_UNAVAILABLE()
+        }
+        
+        os_log(.fault, dso: handle, log: log, warningFormat, vaList)
+    }
+}
+
+extension XcodeRuntimeIssueLogger {
+    @_transparent
+    public func error(_ error: Error) {
+        runtimeIssue("%{public}@", error.localizedDescription)
+    }
+}
+
+// MARK: - Supplementary API
+
+@_transparent
+public func runtimeIssue(
+    _ warningFormat: StaticString,
+    file: StaticString = #file,
+    line: UInt = #line,
+    _ arguments: CVarArg...
+) {
+    XcodeRuntimeIssueLogger.default.raise(
+        warningFormat,
+        file: file,
+        line: line,
+        vaList: arguments
+    )
+}
+
+@_transparent
+public func runtimeIssue(
+    _ message: @autoclosure () -> String,
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    XcodeRuntimeIssueLogger.default.raise(
+        "%@",
+        file: file,
+        line: line,
+        vaList: message()
+    )
+}
+
+// MARK: - Auxiliary
+
+extension XcodeRuntimeIssueLogger {
+    public static let systemFrameworkHandle: UnsafeRawPointer? = {
+        for i in 0..<_dyld_image_count() {
+            // Technically any system framework would work, but this was inspired by SwiftUI's use
+            // of runtime issues to report non-fatal but unexpected behavior.
+            guard let name = _dyld_get_image_name(i).flatMap(String.init(utf8String:)), name.contains("SwiftUI") else { continue }
+            return UnsafeRawPointer(_dyld_get_image_header(i))
+        }
+        return nil
+    }()
+    
+    public class CallsiteCache {
+        private struct Invocation: Hashable {
+            var file: HashedStaticString
+            var line: UInt
+        }
+        
+        private var invocations = Set<Invocation>()
+        
+        /// Returns whether to raise a runtime issue in a file on a particular line.
+        ///
+        /// A notification of a runtime issue will only arise once, so only the first call will return true.
+        public func shouldRaiseIssue(
+            in file: StaticString,
+            on line: UInt
+        ) -> Bool {
+            return invocations.insert(Invocation(file: HashedStaticString(file), line: line)).inserted
+        }
+    }
+}
+
+private var hasLoggedUnavailable = false
+
+public func RTI_RUNTIME_ISSUES_UNAVAILABLE() {
+    if hasLoggedUnavailable {
+        return
+    }
+    
+    os_log(.fault, "Warn only once: a runtime issue logging expectation was violated. Runtime issues will not be logged. Set a symbolic breakpoint on 'RTI_RUNTIME_ISSUES_UNAVAILABLE' to trace.")
+    
+    hasLoggedUnavailable = true
+}
+
+struct HashedStaticString: Hashable {
+    private let base: StaticString
+    
+    init(_ base: StaticString) {
+        self.base = base
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        base.withUTF8Buffer { buffer in
+            hasher.combine(bytes: UnsafeRawBufferPointer(buffer))
+        }
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.base.withUTF8Buffer { lhs in
+            rhs.base.withUTF8Buffer { rhs in
+                zip(lhs, rhs).first(where: { $0.0 != $0.1 }) == nil
+            }
+        }
+    }
+}
