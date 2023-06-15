10d527e1463930332f973e19e0705ed3780ba4e4
Vatsal Manot <vatsal.manot@yahoo.com>
2023-06-15 03:18:35 -0400

    Update package

--- Sources/Swallow/Intermodular/Extensions/Swift/ExpressibleByArrayLiteral++.swift
+++ Sources/Swallow/Intermodular/Extensions/Swift/ExpressibleByArrayLiteral++.swift
@@ -7,7 +7,7 @@ import Swift
 extension ExpressibleByArrayLiteral {
     /// Creates an instance initialized with the given elements.
     public init(_arrayLiteral elements: [Self.ArrayLiteralElement]) {
-        self = _isovariaidc(Self.init(arrayLiteral:))(elements)
+        self = _isovariadic(Self.init(arrayLiteral:))(elements)
     }
     
     public init(_emptyArrayLiteral: Void) {
--- Sources/Swallow/Intramodular/Miscellaneous/Higher Order Functions/variadic.swift
+++ Sources/Swallow/Intramodular/Miscellaneous/Higher Order Functions/variadic.swift
@@ -4,30 +4,30 @@
 
 import Swift
 
-public func _isovariaidc<T, U>(_ f: (@escaping (T...) -> U)) -> (([T]) -> U) {
+public func _isovariadic<T, U>(_ f: (@escaping (T...) -> U)) -> (([T]) -> U) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V>(_ f: (@escaping (T, U...) -> V)) -> ((T, [U]) -> V) {
+public func _isovariadic<T, U, V>(_ f: (@escaping (T, U...) -> V)) -> ((T, [U]) -> V) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V, W>(_ f: (@escaping (T, U, V...) -> W)) -> ((T, U, [V]) -> W) {
+public func _isovariadic<T, U, V, W>(_ f: (@escaping (T, U, V...) -> W)) -> ((T, U, [V]) -> W) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V, W, X>(_ f: (@escaping (T, U, V, W...) -> X)) -> ((T, U, V, [W]) -> X) {
+public func _isovariadic<T, U, V, W, X>(_ f: (@escaping (T, U, V, W...) -> X)) -> ((T, U, V, [W]) -> X) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V, W, X, Y>(_ f: (@escaping (T, U, V, W, X...) -> Y)) -> ((T, U, V, W, [X]) -> Y) {
+public func _isovariadic<T, U, V, W, X, Y>(_ f: (@escaping (T, U, V, W, X...) -> Y)) -> ((T, U, V, W, [X]) -> Y) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V, W, X, Y, Z>(_ f: (@escaping (T, U, V, W, X, Y...) -> Z)) -> ((T, U, V, W, X, [Y]) -> Z) {
+public func _isovariadic<T, U, V, W, X, Y, Z>(_ f: (@escaping (T, U, V, W, X, Y...) -> Z)) -> ((T, U, V, W, X, [Y]) -> Z) {
     unsafeBitCast(f)
 }
 
-public func _isovariaidc<T, U, V>(_ f: (@escaping (inout T) -> ((U...) -> V))) -> ((inout T) -> (([U]) -> V)) {
+public func _isovariadic<T, U, V>(_ f: (@escaping (inout T) -> ((U...) -> V))) -> ((inout T) -> (([U]) -> V)) {
     unsafeBitCast(f)
 }
