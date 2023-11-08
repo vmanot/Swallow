9e01514c82124281be80cad0dde2cbac4ceb714a
Vatsal Manot <vatsal.manot@yahoo.com>
2023-11-08 01:56:38 -0800

    Update package

---
+++ Sources/Diagnostics/Intramodular/Terminal UI/TerminalUI.swift
@@ -0,0 +1,108 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Foundation
+import Swift
+
+/// Terminal UI.
+@_spi(Internal)
+public enum TerminalUI {
+    public enum ASCII {
+        
+    }
+}
+
+@_spi(Internal)
+extension TerminalUI.ASCII {
+    public enum BoxContentAlignment {
+        case left
+        case center
+        case right
+    }
+}
+
+@_spi(Internal)
+public func _printEnclosedInASCIIBox(
+    _ input: String,
+    terminalWidth: Int = 80,
+    alignment: TerminalUI.ASCII.BoxContentAlignment = .left
+) {
+    let horizontalBorder = "+" + String(repeating: "-", count: terminalWidth - 2) + "+"
+    let wordWrapWidth = terminalWidth - 4
+    
+    func wrap(_ text: String, width: Int) -> [String] {
+        var lines: [String] = []
+        var line = ""
+        
+        text
+            .split(
+                separator: " ",
+                omittingEmptySubsequences: true
+            )
+            .forEach { (word: Substring) in
+                if word.count > width {
+                    // Split the long word itself if it's longer than the wrap width
+                    let splitWordIndex = word.index(word.startIndex, offsetBy: width)
+                    let firstPart = word[..<splitWordIndex]
+                    let secondPart = word[splitWordIndex...]
+                    if !line.isEmpty {
+                        lines.append(String(line))
+                        line = ""
+                    }
+                    lines.append(String(firstPart)) // Add the first part as a new line
+                    line = String(secondPart) // Start with the second part on the next line
+                } else if line.count + word.count + 1 > width {
+                    lines.append(line)
+                    line = String(word)
+                } else {
+                    line += (line.isEmpty ? "" : " ") + String(word)
+                }
+            }
+        
+        if !line.isEmpty {
+            lines.append(line)
+        }
+        
+        return lines
+    }
+    
+    func printLine(_ line: String, alignment: TerminalUI.ASCII.BoxContentAlignment) {
+        let padding = wordWrapWidth - line.count
+        let paddingLeft: Int
+        let paddingRight: Int
+        
+        switch alignment {
+            case .left:
+                paddingLeft = 0
+                paddingRight = padding
+            case .center:
+                paddingLeft = padding / 2
+                paddingRight = padding - paddingLeft
+            case .right:
+                paddingLeft = padding
+                paddingRight = 0
+        }
+        
+        let paddedLine = "| " + String(repeating: " ", count: paddingLeft) +
+        
+        line + String(repeating: " ", count: paddingRight) + " |"
+        
+        print(paddedLine)
+    }
+    
+    let lines = input
+        .components(
+            separatedBy: .newlines
+        ).flatMap {
+            wrap($0, width: wordWrapWidth)
+        }
+    
+    print(horizontalBorder)
+    
+    lines.forEach {
+        printLine($0, alignment: alignment)
+    }
+    
+    print(horizontalBorder)
+}
