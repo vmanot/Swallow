9933e2589df1965a107c2d31668aafeaac968788
Vatsal Manot <vatsal.manot@yahoo.com>
2023-11-21 18:26:49 -0800

    Update package

---
+++ Sources/Swallow/Intermodular/Helpers/_Concurrency/_TaskLocalValues.swift
@@ -0,0 +1,13 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+
+import Foundation
+
+/// A generic namespace for `@TaskLocal` values since "Property wrappers are not yet supported in top-level code" nor computed properties.
+///
+/// Why must I invent all the sensible namespaces... *sigh*
+public enum _TaskLocalValues {
+    
+}
