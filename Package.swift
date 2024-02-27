fb4f7b5c8676dce4a0f5956984cd5950d9a51861
John Holdsworth <>
2024-02-27 13:45:29 +0100

    Generate framework

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     products: [
         .library(
             name: "Swallow",
+            type: .dynamic,
             targets: [
                 "_ExpansionsRuntime",
                 "SE0270_RangeSet",
