9acbf7644848a3c4bc72aa4259f61d9738fff840
Vatsal Manot <vatsal.manot@yahoo.com>
2024-04-30 22:03:39 -0700

    Update package

--- Sources/_SwallowSwiftOverlay/Intermodular/Swift/Collection++.swift
+++ Sources/_SwallowSwiftOverlay/Intermodular/Swift/Collection++.swift
@@ -12,3 +12,39 @@ extension Collection {
         Swift.zip(indices, self)
     }
 }
+
+extension Collection {
+    public func lazySubsequences(
+        matching predicate: @escaping (Element) -> Bool
+    ) -> LazySequence<UnfoldSequence<SubSequence, Index>> {
+        return sequence(state: startIndex) { index -> SubSequence? in
+            guard let startIndex = self[index...].firstIndex(where: predicate) else {
+                return nil
+            }
+            
+            let endIndex = self[startIndex...].firstIndex(where: { !predicate($0) }) ?? self.endIndex
+            
+            index = endIndex
+            
+            return self[startIndex..<endIndex]
+        }
+        .lazy
+    }
+    
+    public func enumerateLazySubsequences(
+        matching predicate: @escaping (Element) -> Bool
+    ) -> LazySequence<UnfoldSequence<(Range<Index>, SubSequence), Index>> {
+        return sequence(state: startIndex) { index -> (Range<Index>, SubSequence)? in
+            guard let startIndex = self[index...].firstIndex(where: predicate) else {
+                return nil
+            }
+            
+            let endIndex = self[startIndex...].firstIndex(where: { !predicate($0) }) ?? self.endIndex
+            
+            index = endIndex
+            
+            return (startIndex..<endIndex, self[startIndex..<endIndex])
+        }
+        .lazy
+    }
+}
