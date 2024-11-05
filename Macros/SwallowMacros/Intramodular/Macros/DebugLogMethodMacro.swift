7968bb5ae16452751212909cee41fc706c06d40d
Akshat Patel <akshat.patel16@gmail.com>
2024-11-05 11:13:23 +0530

    Print parameters

--- Macros/SwallowMacros/Intramodular/Macros/DebugLogMethodMacro.swift
+++ Macros/SwallowMacros/Intramodular/Macros/DebugLogMethodMacro.swift
@@ -11,20 +11,41 @@ public struct DebugLogMethodMacro: BodyMacro {
         providingBodyFor declaration: some SwiftSyntax.DeclSyntaxProtocol & SwiftSyntax.WithOptionalCodeBlockSyntax,
         in context: some SwiftSyntaxMacros.MacroExpansionContext
     ) throws -> [SwiftSyntax.CodeBlockItemSyntax] {
-        guard let declaration = declaration.as(FunctionDeclSyntax.self) else {
-            return []
-        }
-
         guard let originalBody = declaration.body?.statements else {
             return []
         }
         
-        let methodName = declaration.name.text
-        var newBody: [CodeBlockItemSyntax] = [
-            "print(\"Entering method \(raw: methodName)\")"
-        ]
-        newBody.append(contentsOf: originalBody)
-        newBody.append("print(\"Exiting method \(raw: methodName)\")")
-        return newBody
+        if let declaration = declaration.as(FunctionDeclSyntax.self) {
+            let methodName = declaration.name.text
+            let parameters = declaration.signature.parameterClause.parameters
+            
+            var newBody: [CodeBlockItemSyntax] = [
+                "print(\"Entering method \(raw: methodName)\")"
+            ]
+            
+            if !parameters.isEmpty {
+                newBody.append("print(\"Parameters:\")")
+                for param in parameters {
+                    let paramName = param.secondName?.text ?? param.firstName.text
+                    newBody.append("print(\"\(raw: paramName): \\(\(raw: paramName))\")")
+                }
+            }
+            
+            newBody.append(contentsOf: originalBody)
+            newBody.append("print(\"Exiting method \(raw: methodName)\")")
+            
+            return newBody
+        } else if let declaration = declaration.as(AccessorDeclSyntax.self) {
+            var newBody: [CodeBlockItemSyntax] = [
+                "print(\"Entering method \(raw: declaration.accessorSpecifier.text)\")"
+            ]
+            newBody.append(contentsOf: originalBody)
+            newBody.append("print(\"Exiting method \(raw: declaration.accessorSpecifier.text)\")")
+            return newBody
+        } else {
+            var newBody: [CodeBlockItemSyntax] = []
+            newBody.append(contentsOf: originalBody)
+            return newBody
+        }
     }
 }
--- Tests/Swallow/DebugLogMethodMacroTests.swift
+++ Tests/Swallow/DebugLogMethodMacroTests.swift
@@ -14,6 +14,9 @@ final class DebugLogMethodMacroTests: XCTestCase {
         testClass.debugTestOne(x: 10, y: "20")
         // Shouldn't print
         testClass.debugTestTwo(x: 20, y: "40")
+        // Should print
+        let z = TestStruct(a: 30, b: "New", c: CGPoint(x: 10, y: 10))
+        testClass.debugTestThree(x: 10, y: "20", z: z)
     }
     
     func testDebugLogMethodMacroExpansion() {
@@ -27,7 +30,59 @@ final class DebugLogMethodMacroTests: XCTestCase {
             expandedSource: """
             func test(x: Int) {
                 print("Entering method test")
+                print("Parameters:")
+                print("x: \\(x)")
                 let y = x + 1
+                print("Exiting method test")
+            }
+            """,
+            macros: ["_DebugLogMethod": DebugLogMethodMacro.self]
+        )
+    }
+    
+    func testDebugLogMethodMacroExpansionWithObjects() {
+        assertMacroExpansion(
+            """
+            @_DebugLogMethod
+            func test(x: Int, y: String, z: TestStruct) {
+            }
+            """,
+            expandedSource: """
+            func test(x: Int, y: String, z: TestStruct) {
+                print("Entering method test")
+                print("Parameters:")
+                print("x: \\(x)")
+                print("y: \\(y)")
+                print("z: \\(z)")
+                print("Exiting method test")
+            }
+            """,
+            macros: ["_DebugLogMethod": DebugLogMethodMacro.self]
+        )
+    }
+    
+    func testDebugLogMethodMacroExpansionBeforeReturn() {
+        assertMacroExpansion(
+            """
+            @_DebugLogMethod
+            func test(x: Int) -> String {
+                if (x % 2 == 0) {
+                    return "Even"
+                } else {
+                    return "Odd"
+                }
+            }
+            """,
+            expandedSource: """
+            func test(x: Int) -> String {
+                print("Entering method test")
+                print("Parameters:")
+                print("x: \\(x)")
+                if (x % 2 == 0) {
+                        return "Even"
+                    } else {
+                        return "Odd"
+                    }
                 print("Exiting method test")
             }
             """,
@@ -51,10 +106,51 @@ final class DebugLogMethodMacroTests: XCTestCase {
             macros: ["_DebugLogMethod": DebugLogMethodMacro.self]
         )
     }
+    
+    /// Test getter and setter of computed property
+    func testDebugLogMethodMacroWithExplicitGetterAndSetter() {
+        assertMacroExpansion(
+            """
+            var computedProperty: Int {
+                @_DebugLogMethod
+                get {
+                    return 42
+                }
+                @_DebugLogMethod
+                set {
+                    print("Inside setter")
+                }
+            }
+            """,
+            expandedSource: """
+            var computedProperty: Int {
+                get {
+                    print("Entering method get")
+                    return 42
+                    print("Exiting method get")
+                }
+                set {
+                    print("Entering method set")
+                    print("Inside setter")
+                    print("Exiting method set")
+                }
+            }
+            """,
+            macros: ["_DebugLogMethod": DebugLogMethodMacro.self]
+        )
+    }
+}
+
+fileprivate struct TestStruct {
+    let a: Int
+    let b: String
+    let c: CGPoint
 }
 
 fileprivate final class TestClass {
     @_DebugLogMethod
     func debugTestOne(x: Int, y: String) {}
     func debugTestTwo(x: Int, y: String) {}
+    @_DebugLogMethod
+    func debugTestThree(x: Int, y: String, z: TestStruct) {}
 }
